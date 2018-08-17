2babfc2908f3b1a92e89ad470774d6d6a0574b41
Mauro Guerrera <stark.devel@gmail.com>
2018-08-17 21:26:13 +0200

    Update UART RX controller

--- vhd/src/uart/uart-rx.vhd
+++ vhd/src/uart/uart-rx.vhd
@@ -11,6 +11,7 @@ entity UART_RX is
     g_baud_rate   : natural := 9600;
     g_databits    : natural := 8;
     g_parity      : boolean := false;
+    g_parity_odd  : boolean := false;
     g_stop_bits   : integer := 1
   );
   port (
@@ -19,7 +20,8 @@ entity UART_RX is
     i_uart_in     : in    std_logic;  -- UART IN port
     i_clk         : in    std_logic;  -- external clock
     o_uart_data   : out   std_logic_vector(g_databits-1 downto 0);
-    o_uart_end    : out   std_logic
+    o_uart_end    : out   std_logic;
+    o_uart_err    : out   std_logic
   );
 end UART_RX;
 
@@ -39,6 +41,17 @@ architecture RTL of UART_RX is
       o_tc        : out  std_logic);
   end component;
 
+  component SHIFT_REGISTER is
+  generic (
+    N           : integer := 16);
+  port (
+    i_clk       : in   std_logic;
+    i_reset_n   : in   std_logic;
+    i_shift_en  : in   std_logic;
+    i_serial_in : in   std_logic;
+    o_par_out   : out  std_logic_vector(N-1 downto 0));
+  end component;
+
 --------------------------------------------------------------------------------
 -- types
 --------------------------------------------------------------------------------
@@ -47,17 +60,21 @@ architecture RTL of UART_RX is
 --------------------------------------------------------------------------------
 -- constants
 --------------------------------------------------------------------------------
-  constant c_clock_divider : natural := g_ext_clock / (g_baud_rate * 2);
+  constant c_clock_divider : natural := (g_ext_clock / g_baud_rate) - 1;
+  constant c_half_period   : natural := c_clock_divider / 2;
 
 --------------------------------------------------------------------------------
 -- signals
 --------------------------------------------------------------------------------
 
-  -- clock counter
-  signal s_cl_cnt_en        : std_logic;
-  signal s_cl_cnt_rst       : std_logic;
-  signal s_cl_cnt_tc        : std_logic;
-  signal s_cl_cnt_out       : unsigned(up_int_log2(c_clock_divider)-1 downto 0);
+  -- rx bit counter
+  signal s_rx_cnt_en        : std_logic;
+  signal s_rx_cnt_rst       : std_logic;
+  signal s_rx_cnt_tc        : std_logic;
+  signal s_rx_cnt_out       : unsigned(up_int_log2(c_clock_divider)-1 downto 0);
+
+  signal s_zero_cnt_evt     : std_logic;
+  signal s_half_cnt_evt     : std_logic;
 
   -- data counter
   signal s_dt_cnt_en        : std_logic;
@@ -65,24 +82,40 @@ architecture RTL of UART_RX is
   signal s_dt_cnt_tc        : std_logic;
   signal s_dt_cnt_out       : unsigned(up_int_log2(g_databits)-1 downto 0);
 
+  -- parity bit register
+  signal s_par_rg_en        : std_logic;
+  signal s_par_rg_rst       : std_logic;
+  signal s_par_rg_val       : std_logic;
+
+  -- uart data shift reg
+  signal s_sr_sh_en         : std_logic;
+  signal s_sr_rst           : std_logic;
+  signal s_sr_data_out      : std_logic_vector(g_databits-1 downto 0);
+
   -- uart
   signal s_uart_state   : t_uart_st;
   signal s_uart_clk     : std_logic;
-  signal s_uart_data    : std_logic_vector(g_databits-1 downto 0);
+  signal s_uart_clk_n   : std_logic;
+
+  signal s_data_end     : std_logic;
+
+  signal s_par_check    : std_logic;
+  signal s_par_error    : std_logic;
+  signal s_par_bit      : std_logic;
 
 begin
 
-  uart_clock_cnt : UP_COUNTER
+  uart_rx_bit_cnt : UP_COUNTER
   generic map (
     up_int_log2(c_clock_divider)
   )
   port map (
     i_clk       => i_clk,
-    i_reset_n   => s_cl_cnt_rst,
-    i_en        => s_cl_cnt_en,
+    i_reset_n   => s_rx_cnt_rst,
+    i_en        => s_rx_cnt_en,
     i_tc_value  => to_unsigned(c_clock_divider, up_int_log2(c_clock_divider)),
-    o_q         => s_cl_cnt_out,
-    o_tc        => s_cl_cnt_tc
+    o_q         => s_rx_cnt_out,
+    o_tc        => s_rx_cnt_tc
   );
 
   uart_data_cnt : UP_COUNTER
@@ -98,12 +131,44 @@ begin
     o_tc        => s_dt_cnt_tc
   );
 
-  p_uart_clock: process(i_uart_rst_n, s_cl_cnt_tc)
+  uart_data_sr : SHIFT_REGISTER
+  generic map (
+    g_databits
+  )
+  port map (
+    i_clk       => s_uart_clk_n,
+    i_reset_n   => s_sr_rst,
+    i_shift_en  => s_sr_sh_en,
+    i_serial_in => i_uart_in,
+    o_par_out   => s_sr_data_out
+  );
+
+  p_zero_cnt_evt: process(s_rx_cnt_out)
   begin
-    if i_uart_rst_n = '0' then
+    if s_rx_cnt_out = to_unsigned(0, up_int_log2(c_clock_divider)) then
+      s_zero_cnt_evt <= '1';
+    else
+      s_zero_cnt_evt <= '0';
+    end if;
+  end process;
+
+  p_half_cnt_evt: process(s_rx_cnt_out)
+  begin
+    if s_rx_cnt_out = to_unsigned(c_half_period, up_int_log2(c_clock_divider)) then
+      s_half_cnt_evt <= '1';
+    else
+      s_half_cnt_evt <= '0';
+    end if;
+  end process;
+
+  p_uart_clock: process(s_rx_cnt_rst, s_zero_cnt_evt, s_half_cnt_evt)
+  begin
+    if s_rx_cnt_rst = '0' then
+      s_uart_clk <= '0';
+    elsif s_zero_cnt_evt'event and s_zero_cnt_evt = '1' then
       s_uart_clk <= '1';
-    elsif(s_cl_cnt_tc'event and s_cl_cnt_tc = '1') then
-      s_uart_clk <= not(s_uart_clk);
+    elsif s_half_cnt_evt'event and s_half_cnt_evt = '1' then
+      s_uart_clk <= '0';
     end if;
   end process;
 
@@ -155,70 +220,158 @@ begin
   begin
     case s_uart_state is
       when st_idle    =>
-        s_cl_cnt_en  <= '0';
-        s_cl_cnt_rst <= '0';
+        s_rx_cnt_en  <= '0';
+        s_rx_cnt_rst <= '0';
 
         s_dt_cnt_en  <= '0';
         s_dt_cnt_rst <= '0';
 
-        o_uart_end   <= '0';
+        s_sr_rst     <= '0';
+        s_sr_sh_en   <= '0';
+
+        s_par_check  <= '0';
+        s_par_rg_en  <= '0';
+        s_par_rg_rst <= '0';
+
+        s_data_end   <= '0';
       when st_start   =>
-        s_cl_cnt_en  <= '1';
-        s_cl_cnt_rst <= '1';
+        s_rx_cnt_en  <= '1';
+        s_rx_cnt_rst <= '1';
 
         s_dt_cnt_en  <= '0';
         s_dt_cnt_rst <= '0';
 
-        o_uart_end   <= '0';
+        s_sr_rst     <= '0';
+        s_sr_sh_en   <= '0';
+
+        s_par_check  <= '0';
+        s_par_rg_en  <= '0';
+        s_par_rg_rst <= '1';
+
+        s_data_end   <= '0';
       when st_data    =>
-        s_cl_cnt_en  <= '1';
-        s_cl_cnt_rst <= '1';
+        s_rx_cnt_en  <= '1';
+        s_rx_cnt_rst <= '1';
 
         s_dt_cnt_en  <= '1';
         s_dt_cnt_rst <= '1';
 
-        o_uart_end   <= '0';
+        s_sr_rst     <= '1';
+        s_sr_sh_en   <= '1';
+
+        s_par_check  <= '0';
+        s_par_rg_en  <= '0';
+        s_par_rg_rst <= '1';
+
+        s_data_end   <= '0';
       when st_parity =>
-        s_cl_cnt_en  <= '1';
-        s_cl_cnt_rst <= '1';
+        s_rx_cnt_en  <= '1';
+        s_rx_cnt_rst <= '1';
 
         s_dt_cnt_en  <= '0';
         s_dt_cnt_rst <= '1';
 
-        o_uart_end   <= '0';
+        s_sr_rst     <= '1';
+        s_sr_sh_en   <= '0';
+
+        s_par_check  <= '0';
+        s_par_rg_en  <= '1';
+        s_par_rg_rst <= '1';
+
+        s_data_end   <= '0';
       when st_stop1   =>
-        s_cl_cnt_en  <= '1';
-        s_cl_cnt_rst <= '1';
-
-        s_dt_cnt_en  <= '0';
-        s_dt_cnt_rst <= '1';
-
-        o_uart_end   <= '0';
-      when st_stop2   =>
-        s_cl_cnt_en  <= '1';
-        s_cl_cnt_rst <= '1';
-
-        s_dt_cnt_en  <= '0';
-        s_dt_cnt_rst <= '1';
-
-        o_uart_end   <= '0';
-      when st_end     =>
-        s_cl_cnt_en  <= '1';
-        s_cl_cnt_rst <= '1';
-
-        s_dt_cnt_en  <= '0';
-        s_dt_cnt_rst <= '1';
-
-        o_uart_end   <= '1';
-      when others    =>
-        s_cl_cnt_en  <= '0';
-        s_cl_cnt_rst <= '0';
+        s_rx_cnt_en  <= '1';
+        s_rx_cnt_rst <= '1';
 
         s_dt_cnt_en  <= '0';
         s_dt_cnt_rst <= '0';
 
-        o_uart_end   <= '0';
+        s_sr_rst     <= '1';
+        s_sr_sh_en   <= '0';
+
+        s_par_check  <= '0';
+        s_par_rg_en  <= '0';
+        s_par_rg_rst <= '1';
+
+        s_data_end   <= '0';
+      when st_stop2   =>
+        s_rx_cnt_en  <= '1';
+        s_rx_cnt_rst <= '1';
+
+        s_dt_cnt_en  <= '0';
+        s_dt_cnt_rst <= '0';
+
+        s_sr_rst     <= '1';
+        s_sr_sh_en   <= '0';
+
+        s_par_check  <= '0';
+        s_par_rg_en  <= '0';
+        s_par_rg_rst <= '1';
+
+        s_data_end   <= '0';
+      when st_end     =>
+        s_rx_cnt_en  <= '1';
+        s_rx_cnt_rst <= '1';
+
+        s_dt_cnt_en  <= '0';
+        s_dt_cnt_rst <= '0';
+
+        s_sr_rst     <= '1';
+        s_sr_sh_en   <= '0';
+
+        if g_parity = true then
+          s_par_check  <= '1';
+        else
+          s_par_check  <= '0';
+        end if;
+        s_par_rg_en  <= '0';
+        s_par_rg_rst <= '1';
+
+        s_data_end   <= '1';
+      when others    =>
+        s_rx_cnt_en  <= '0';
+        s_rx_cnt_rst <= '0';
+
+        s_dt_cnt_en  <= '0';
+        s_dt_cnt_rst <= '0';
+
+        s_sr_rst     <= '0';
+        s_sr_sh_en   <= '0';
+
+        s_par_check  <= '0';
+        s_par_rg_en  <= '0';
+        s_par_rg_rst <= '0';
+
+        s_data_end   <= '0';
     end case;
   end process;
+
+  p_parity_check_reg: process(s_par_rg_en, s_par_rg_rst, s_uart_clk_n)
+  begin
+    if s_par_rg_rst = '0' then
+      s_par_rg_val <= '0';
+    elsif s_par_rg_en ='1' then
+      if s_uart_clk_n'event and s_uart_clk_n = '1' then
+        s_par_rg_val <= i_uart_in;
+      end if;
+    end if;
+  end process;
+
+
+  p_parity_check: process(s_par_rg_val, s_par_bit)
+  begin
+    if g_parity_odd = false then
+      s_par_error <= s_par_bit xor s_par_rg_val;
+    else
+      s_par_error <= s_par_bit xor not(s_par_rg_val);
+    end if;
+  end process;
+
+  s_uart_clk_n  <= not(s_uart_clk);
+  s_par_bit     <= xor_reduce(s_sr_data_out);
+
+  o_uart_data   <= s_sr_data_out;
+  o_uart_end    <= s_data_end;
+  o_uart_err    <= s_par_check and s_par_error;
 
 end architecture RTL;
--- vhd/tb/tb_uart-rx.vhd
+++ vhd/tb/tb_uart-rx.vhd
@@ -16,6 +16,7 @@ architecture TEST of TB_UART_RX is
       g_baud_rate   : natural := 9600;
       g_databits    : natural := 8;
       g_parity      : boolean := false;
+      g_parity_odd  : boolean := false;
       g_stop_bits   : integer := 1
     );
     port (
@@ -24,28 +25,33 @@ architecture TEST of TB_UART_RX is
       i_uart_in     : in    std_logic;  -- UART IN port
       i_clk         : in    std_logic;  -- external clock
       o_uart_data   : out   std_logic_vector(g_databits-1 downto 0);
-      o_uart_end    : out   std_logic
+      o_uart_end    : out   std_logic;
+      o_uart_err    : out   std_logic
     );
   end component;
 
-  constant c_clock_period : time := 1 ns;
+  constant c_clock_period : time := 10 ns;
+  constant c_uart_period  : time := 8681 ns;
+  constant c_databits     : integer := 8;
 
   signal s_uart_en    : std_logic;
   signal s_uart_rst_n : std_logic;
   signal s_uart_in    : std_logic;
   signal s_clk        : std_logic;
-  signal s_uart_data  : std_logic_vector(6 downto 0);
+  signal s_uart_data  : std_logic_vector(c_databits-1 downto 0);
   signal s_uart_end   : std_logic;
+  signal s_uart_err   : std_logic;
 
 begin
 
   DUT: UART_RX
     generic map (
       g_ext_clock  => 50000000,
-      g_baud_rate  => 5000000,
-      g_databits   => 7,
-      g_parity     => false,
-      g_stop_bits  => 1
+      g_baud_rate  => 115200,
+      g_databits   => c_databits,
+      g_parity     => true,
+      g_parity_odd => false,
+      g_stop_bits  => 2
     )
     port map (
       i_uart_en     => s_uart_en,
@@ -53,7 +59,8 @@ begin
       i_uart_in     => s_uart_in,
       i_clk         => s_clk,
       o_uart_data   => s_uart_data,
-      o_uart_end    => s_uart_end
+      o_uart_end    => s_uart_end,
+      o_uart_err    => s_uart_err
     );
 
   clock_gen : process
@@ -71,7 +78,7 @@ begin
     s_uart_in    <= '1';
     wait for 1 ns;
 
-    s_uart_en    <= '1';
+    s_uart_en    <= '0';
     s_uart_rst_n <= '0';
     s_uart_in    <= '1';
     wait for 10 ns;
@@ -79,13 +86,143 @@ begin
     s_uart_en    <= '1';
     s_uart_rst_n <= '1';
     s_uart_in    <= '1';
-    wait for 10 ns;
+    wait for 9 ns;
 
     s_uart_en    <= '1';
     s_uart_rst_n <= '1';
-    s_uart_in    <= '0';
-    wait for 10 ns;
-    -- assert t_data_out = "11001100101111101011110000100000" report "Error -99999999";
+    s_uart_in    <= '0';  --start bit
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '1';  -- data 0
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '1';  -- data 1
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '0';  -- data 2
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '1';  -- data 3
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '0';  -- data 4
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '0';  -- data 5
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '1';  -- data 6
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '0';  -- data 7
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '0';  -- parity (even parity)
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '1';  -- stop 1
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '1';  -- stop 2
+    wait for c_uart_period;
+
+    -- assert s_uart_data = "11010010" report "Error UART data 11010010";
+    -- assert s_uart_err = '0' report "Error UART parity check 0";
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '1';  -- idle
+    wait for 3*c_uart_period;
+
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '0';  --start bit
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '0';  -- data 0
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '0';  -- data 1
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '1';  -- data 2
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '0';  -- data 3
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '1';  -- data 4
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '1';  -- data 5
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '1';  -- data 6
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '1';  -- data 7
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '1';  -- parity (even parity)
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '1';  -- stop 1
+    wait for c_uart_period;
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '1';  -- stop 2
+    wait for c_uart_period;
+
+    -- assert s_uart_data = "00101111" report "Error UART data 00101111";
+    -- assert s_uart_err = '1' report "Error UART parity check 1";
+
+    s_uart_en    <= '1';
+    s_uart_rst_n <= '1';
+    s_uart_in    <= '1';  -- idle
 
     wait;
   end process;
