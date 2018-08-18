6d27320161b20f0cab3257aaa916c7d2fd4b823e
Mauro Guerrera <stark.devel@gmail.com>
2018-08-18 11:23:27 +0200

    Update shift register

--- vhd/src/basics/r_shift_reg.vhd
+++ vhd/src/basics/r_shift_reg.vhd
@@ -3,7 +3,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity SHIFT_REGISTER is
+entity R_SHIFT_REGISTER is
 generic (
   N           : integer := 16);
 port (
@@ -14,7 +14,7 @@ port (
   o_par_out   : out  std_logic_vector(N-1 downto 0));
 end entity;
 
-architecture BHV of SHIFT_REGISTER is
+architecture BHV of R_SHIFT_REGISTER is
 
 signal s_data : std_logic_vector(N-1 downto 0);
 
@@ -26,10 +26,10 @@ begin
   begin
     if i_reset_n = '0' then
       s_data <= (others => '0');
-    elsif i_shift_en = '1' then
-      if i_clk'event and i_clk = '1' then
-        s_data(N-1 downto 1) <= s_data(N-2 downto 0);
-        s_data(0) <= i_serial_in;
+    elsif i_clk'event and i_clk = '1' then
+      if i_shift_en = '1' then
+        s_data(N-2 downto 0) <= s_data(N-1 downto 1);
+        s_data(N-1) <= i_serial_in;
       end if;
     end if;
   end process;
