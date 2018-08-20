e773e21d573902d788b029360ac5c579705b4ec4
Mauro Guerrera <stark.devel@gmail.com>
2018-08-20 20:30:35 +0200

    Add type conversion LUT

---
+++ vhd/src/midi/midi_type_lut.vhd
@@ -0,0 +1,35 @@
+library ieee;
+
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use work.UTILS_PKG.all;
+
+entity MIDI_TYPE_LUT is
+port (
+  i_midi_cmd    : in  t_midi_cmd;
+  o_type        : out std_logic_vector(TYPE_BITS - 1 downto 0)
+  );
+end entity;
+
+architecture BHV of MIDI_TYPE_LUT is
+
+begin
+
+  p_midi_cmd: process(i_midi_cmd)
+  begin
+    case i_midi_cmd is
+      when midi_note_off =>
+        o_type <= "000";
+      when midi_note_on =>
+        o_type <= "001";
+      when midi_ctrl_ch =>
+        o_type <= "010";
+      when midi_prg_ch =>
+        o_type <= "011";
+      when others =>
+        o_type <= "111";
+      end case;
+  end process;
+
+end architecture;
