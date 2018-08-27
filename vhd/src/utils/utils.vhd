2407a1c7b853dc66b55ceed515b0404938f9852d
Mauro Guerrera <stark.devel@gmail.com>
2018-08-28 01:25:35 +0200

    Add midi ts type

--- vhd/src/utils/utils.vhd
+++ vhd/src/utils/utils.vhd
@@ -72,6 +72,7 @@ package UTILS_PKG is
 
   type t_midi_ready     is array (SEQ_TRACKS - 1 downto 0) of std_logic;
   type t_midi_data      is array (SEQ_TRACKS - 1 downto 0) of std_logic_vector(SEQ_EVENT_SIZE - 1  downto 0);
+  type t_midi_ts        is array (SEQ_TRACKS - 1 downto 0) of std_logic_vector(SEQ_TIME_SIZE - 1  downto 0);
 
   type t_sg_note        is array (SEQ_TRACKS - 1 downto 0) of std_logic_vector(MIDI_DATA_SIZE - 1 downto 0);
   type t_sg_vel         is array (SEQ_TRACKS - 1 downto 0) of std_logic_vector(MIDI_DATA_SIZE - 1 downto 0);
