845919710bcb3ddb3b72229689fad91171dd0441
Mauro Guerrera <stark.devel@gmail.com>
2018-08-27 20:14:56 +0200

    Utils package rework

--- vhd/src/utils/utils.vhd
+++ vhd/src/utils/utils.vhd
@@ -5,7 +5,34 @@ use ieee.numeric_std.all;
 package UTILS_PKG is
 
   -- constants
-  constant DISPLAY_SIZE : integer := 6;
+  constant MIDI_DATA_SIZE : natural := 7;   -- standard MIDI data
+
+  -- sequencer constants (according to internal sequencer structure)
+  constant MIDI_TYPE_SIZE : natural := 3;   -- configurable for future use
+
+  constant SEQ_EVENT_SIZE : natural := 32;  -- size of internally represented MIDI event
+  constant SEQ_TIME_SIZE  : natural := 32;  -- size of internally represented timestamp
+
+  -- sequencer core constants (status register)
+  constant SEQ_TRACKS     : natural := 8;   -- available tracks
+  constant ST_TRACK_SIZE  : natural := 3;   -- bits to represent avail tracks
+
+  constant ST_TSS_SIZE    : natural := 11;  -- timestamp in seconds
+  constant ST_TSF_SIZE    : natural := 12;  -- timestamp in fractions
+  constant ST_RUN_SIZE    : natural := 1;   -- play / pause
+  constant ST_VOL_SIZE    : natural := 7;   -- volume
+  -- constant ST_BPM_SIZE    : natural := 8;   -- bpm tempo
+  -- constant ST_MTR_SIZE    : natural := 1;   -- metronome enable
+
+  -- track status register constants
+  constant TR_ST_SIZE     : natural := 32;
+  constant TR_PATCH_SIZE  : natural := 7;
+  constant TR_CH_SIZE     : natural := 4;
+  constant TR_VOL_SIZE    : natural := 7;
+  constant TR_PAN_SIZE    : natural := 7;
+
+  -- display constants
+  constant DISPLAY_SIZE   : integer := 6;
 
   -- functions
   function log2 (X : positive)  return natural;                   -- Y = log2(X)
@@ -41,46 +68,33 @@ package UTILS_PKG is
     ds_6, ds_7, ds_8, ds_9, ds_OFF
   );
 
-  type t_display_array is array (DISPLAY_SIZE - 1 downto 0) of t_display_out;
+  type t_display_array  is array (DISPLAY_SIZE - 1 downto 0) of t_display_out;
+
+  type t_midi_ready     is array (SEQ_TRACKS - 1 downto 0) of std_logic;
+  type t_midi_data      is array (SEQ_TRACKS - 1 downto 0) of std_logic_vector(SEQ_EVENT_SIZE - 1  downto 0);
+
+  type t_sg_note        is array (SEQ_TRACKS - 1 downto 0) of std_logic_vector(MIDI_DATA_SIZE - 1 downto 0);
+  type t_sg_vel         is array (SEQ_TRACKS - 1 downto 0) of std_logic_vector(MIDI_DATA_SIZE - 1 downto 0);
+  type t_sg_start       is array (SEQ_TRACKS - 1 downto 0) of std_logic;
+  type t_sg_stop        is array (SEQ_TRACKS - 1 downto 0) of std_logic;
 
   -- midi message subtypes (according to MIDI protocol)
   subtype MIDI_CMD_RANGE  is natural range 7 downto 4;
   subtype MIDI_CH_RANGE   is natural range 3 downto 0;
   subtype MIDI_DATA_RANGE is natural range 6 downto 0;
 
-  -- sequencer constants and subtypes (according to internal sequencer structure)
-  constant MIDI_TYPE_SIZE : natural := 3;   -- configurable for future use
-
-  constant SEQ_EVENT_SIZE : natural := 32;  -- size of internally represented MIDI event
-  constant SEQ_TIME_SIZE  : natural := 32;  -- size of internally represented timestamp
-
+  -- sequencer subtypes (according to internal sequencer structure)
   subtype SEQ_RSVD_RANGE  is natural range 31 downto (MIDI_TYPE_SIZE + 18);  -- reserved
   subtype SEQ_TYPE_RANGE  is natural range (MIDI_TYPE_SIZE + 18 - 1) downto 18;
   subtype SEQ_CH_RANGE    is natural range 17 downto 14;
   subtype SEQ_DATA1_RANGE is natural range 13 downto 7;
   subtype SEQ_DATA2_RANGE is natural range 6 downto 0;
 
-  -- sequencer core constants and subtypes (status register)
-  constant SEQ_TRACKS     : natural := 8;   -- available tracks
-  constant ST_TRACK_SIZE  : natural := 3;   -- bits to represent avail tracks
-
-  constant ST_TSS_SIZE    : natural := 11;  -- timestamp in seconds
-  constant ST_TSF_SIZE    : natural := 12;  -- timestamp in fractions
-  constant ST_RUN_SIZE    : natural := 1;   -- play / pause
-  constant ST_VOL_SIZE    : natural := 7;   -- volume
-  -- constant ST_BPM_SIZE    : natural := 8;   -- bpm tempo
-  -- constant ST_MTR_SIZE    : natural := 1;   -- metronome enable
-
+  -- sequencer core subtypes (status register)
   subtype  ST_TSS_RANGE   is natural range (ST_TSS_SIZE + ST_TSF_SIZE - 1) downto ST_TSF_SIZE;
   subtype  ST_TSF_RANGE   is natural range ST_TSF_SIZE  - 1 downto 0;
 
-  -- track status register
-  constant TR_ST_SIZE     : natural := 32;
-  constant TR_PATCH_SIZE  : natural := 7;
-  constant TR_CH_SIZE     : natural := 4;
-  constant TR_VOL_SIZE    : natural := 7;
-  constant TR_PAN_SIZE    : natural := 7;
-
+  -- track status register fields
   subtype  TR_PATCH_RANGE is natural range 6 downto 0;
   subtype  TR_CH_RANGE    is natural range 10 downto 7;
   subtype  TR_VOL_RANGE   is natural range 17 downto 11;
