13c235a578db220255bb2c108aab772070dcd30b
Mauro Guerrera <stark.devel@gmail.com>
2018-09-15 01:10:06 +0200

    Add rec memory init file (all ones)

--- vhd/src/memory/rec_memory.vhd
+++ vhd/src/memory/rec_memory.vhd
@@ -1,7 +1,7 @@
 -- megafunction wizard: %RAM: 1-PORT%
 -- GENERATION: STANDARD
 -- VERSION: WM1.0
--- MODULE: altsyncram 
+-- MODULE: altsyncram
 
 -- ============================================================
 -- File Name: rec_memory.vhd
@@ -19,17 +19,17 @@
 
 
 --Copyright (C) 1991-2013 Altera Corporation
---Your use of Altera Corporation's design tools, logic functions 
---and other software and tools, and its AMPP partner logic 
---functions, and any output files from any of the foregoing 
---(including device programming or simulation files), and any 
---associated documentation or information are expressly subject 
---to the terms and conditions of the Altera Program License 
---Subscription Agreement, Altera MegaCore Function License 
---Agreement, or other applicable license agreement, including, 
---without limitation, that your use is for the sole purpose of 
---programming logic devices manufactured by Altera and sold by 
---Altera or its authorized distributors.  Please refer to the 
+--Your use of Altera Corporation's design tools, logic functions
+--and other software and tools, and its AMPP partner logic
+--functions, and any output files from any of the foregoing
+--(including device programming or simulation files), and any
+--associated documentation or information are expressly subject
+--to the terms and conditions of the Altera Program License
+--Subscription Agreement, Altera MegaCore Function License
+--Agreement, or other applicable license agreement, including,
+--without limitation, that your use is for the sole purpose of
+--programming logic devices manufactured by Altera and sold by
+--Altera or its authorized distributors.  Please refer to the
 --applicable agreement for further details.
 
 
@@ -62,6 +62,8 @@ BEGIN
 	GENERIC MAP (
 		clock_enable_input_a => "BYPASS",
 		clock_enable_output_a => "BYPASS",
+		init_file => "C:/Users/Mauro/Developer/Workspace/Quartus/midi-sequencer/vhd/src/memory/rec_mem.mif",
+		-- init_file => "rec_mem.mif",
 		intended_device_family => "Cyclone V",
 		lpm_hint => "ENABLE_RUNTIME_MOD=NO",
 		lpm_type => "altsyncram",
