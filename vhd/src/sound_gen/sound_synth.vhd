df9d0e1004167e57bdcfd1af32d1de5f166371bd
Mauro Guerrera <stark.devel@gmail.com>
2018-09-15 17:58:23 +0200

    Improve sample sum and add clip logic

--- vhd/src/sound_gen/sound_synth.vhd
+++ vhd/src/sound_gen/sound_synth.vhd
@@ -14,6 +14,8 @@ port (
   i_patch         : in  t_sg_patch;
   i_vol           : in  t_sg_vol;
 
+  i_main_vol      : in  natural range 0 to MAIN_VOL_MAX;
+
   i_sample_clk    : in  std_logic;        -- clock @ sample freq speed
   i_sample_en     : in  t_sample_enable;  -- array of enable signals
   i_sample_idx    : in  t_sample_idx;     -- array of sample indexes
@@ -92,6 +94,12 @@ architecture BHV of SOUND_SYNTH is
   signal s_out_sample_rst       : std_logic;
   signal s_out_sample           : std_logic_vector(SAMPLE_WIDTH - 1 downto 0);
 
+  signal s_out_sample_off       : std_logic_vector(2*SAMPLE_WIDTH - 1 downto 0);
+  signal s_out_sample_lim       : std_logic_vector(2*SAMPLE_WIDTH - 1 downto 0);
+  signal s_out_sample_ext       : std_logic_vector(2*SAMPLE_WIDTH - 1 downto 0);
+
+  signal s_clip                 : std_logic;
+
   -- sample scan counter
   signal s_sample_scan          : unsigned(MAX_POLY_BIT - 1 downto 0);
   signal s_sample_scan_en       : std_logic;
@@ -108,17 +116,54 @@ architecture BHV of SOUND_SYNTH is
 
 begin
 
-  o_clip            <= '0'; -- TODO add clip logic
+  o_clip            <= s_clip;
   o_sample_out      <= s_out_sample;
   o_mem_patch       <= i_patch(to_integer(s_track_scan));
   o_mem_address     <= i_sample_idx(to_integer(s_track_scan))(to_integer(s_sample_scan));
 
   s_sample_scan_end <= to_unsigned(MAX_POLYPHONY - 1, MAX_POLY_BIT);
+  s_sample_offset   <= std_logic_vector(to_unsigned(2**(SAMPLE_WIDTH - 1), 2*SAMPLE_WIDTH));
 
   s_track_scan_en   <= s_sample_scan_tc;
   s_track_scan_end  <= (others => '1');
 
-  s_out_sample_in <= s_current_sample(SAMPLE_WIDTH - 1 downto 0);
+  s_out_sample_in <= s_out_sample_off(SAMPLE_WIDTH - 1 downto 0);
+
+  s_out_sample_off <= std_logic_vector(signed(s_out_sample_lim) + signed(s_sample_offset));
+  s_current_sample_in <= std_logic_vector(signed(s_current_sample) + signed(s_mem_sample_ext));
+
+  p_limiter: process(s_out_sample_ext)
+  begin
+    if s_out_sample_ext(2*SAMPLE_WIDTH - 1) = '1' then  -- negative
+      if and_reduce(s_out_sample_ext(2*SAMPLE_WIDTH - 1 downto SAMPLE_WIDTH - 1)) = '0' then -- overflow
+        s_out_sample_lim(2*SAMPLE_WIDTH - 1 downto SAMPLE_WIDTH - 1)  <= (others => '1');
+        s_out_sample_lim(SAMPLE_WIDTH - 2 downto 0)                   <= (others => '0');
+        s_clip                                                        <= '1';
+      else
+        s_out_sample_lim <= s_out_sample_ext;
+        s_clip                                                        <= '0';
+      end if;
+    else
+      -- positive
+      if or_reduce(s_out_sample_ext(2*SAMPLE_WIDTH - 1 downto SAMPLE_WIDTH - 1)) = '1' then -- overflow
+        s_out_sample_lim(2*SAMPLE_WIDTH - 1 downto SAMPLE_WIDTH - 1)  <= (others => '0');
+        s_out_sample_lim(SAMPLE_WIDTH - 2 downto 0)                   <= (others => '1');
+        s_clip           <= '1';
+      else
+        s_out_sample_lim <= s_out_sample_ext;
+        s_clip           <= '0';
+      end if;
+    end if;
+  end process;
+
+  p_main_vol: process(i_main_vol, s_current_sample)
+  begin
+    if i_main_vol = 0 then
+      s_out_sample_ext <= (others => '0');
+    else
+      s_out_sample_ext <= std_logic_vector(shift_right(signed(s_current_sample), MAIN_VOL_MAX - i_main_vol));
+    end if;
+  end process;
 
   p_mem_sample_ext: process(i_mem_sample, i_vol, s_track_scan)
     variable v_track  : integer := 0;
@@ -130,15 +175,9 @@ begin
       s_mem_sample_ext <= (others => '0');
     else
       s_mem_sample_ext(2*SAMPLE_WIDTH -1 downto SAMPLE_WIDTH - (TR_VOL_MAX - v_vol))  <= (others => i_mem_sample(SAMPLE_WIDTH - 1));
-      s_mem_sample_ext(SAMPLE_WIDTH - (TR_VOL_MAX - v_vol) - 1 downto 0)              <= i_mem_sample(TR_VOL_MAX downto (TR_VOL_MAX - v_vol));
+      s_mem_sample_ext(SAMPLE_WIDTH - (TR_VOL_MAX - v_vol) - 1 downto 0)              <= i_mem_sample(SAMPLE_WIDTH - 1 downto (SAMPLE_WIDTH - 1 - v_vol));
     end if;
   end process;
-
-  -- s_mem_sample_ext(2*SAMPLE_WIDTH - 1 downto SAMPLE_WIDTH) <= (others => i_mem_sample(SAMPLE_WIDTH - 1));
-  -- s_mem_sample_ext(SAMPLE_WIDTH - 1 downto 0) <= i_mem_sample;
-
-  s_sample_offset     <= std_logic_vector(to_unsigned(2**(SAMPLE_WIDTH - 1), 2*SAMPLE_WIDTH));
-  s_current_sample_in <= std_logic_vector(signed(s_current_sample) + signed(s_mem_sample_ext) + signed(s_sample_offset));
 
   p_next_sample_set: process(s_fsm_state, i_sample_clk)
   begin
